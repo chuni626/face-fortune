5d0c22cf04846a815d5d9b2275793061f7d63a70
chuni626 <powerdduksim@gmail.com>
2025-12-31 16:09:57 +0900

    increase body size limit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb', // 1MB 제한을 10MB로 늘립니다!
+    },
+  },
 };
 
 export default nextConfig;
